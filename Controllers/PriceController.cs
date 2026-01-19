bdf5ae3f2fd60d4b1e77b161b31376c03857ac64
Stanislav <sirichenkostas@gmail.com>
2026-01-19 17:22:58 +0200

    Add project files.

---
+++ Controllers/PriceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProfiDental.Controllers;
+
+public class PriceController : Controller
+{
+    [Route("price")]
+    public IActionResult Index()
+    {
+        return this.View("~/Views/Price/Index.cshtml");
+    }
+}
---
+++ Controllers/PrivacyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProfiDental.Controllers;
+
+public class PrivacyController : Controller
+{
+    [Route("privacy")]
+    public IActionResult Index()
+    {
+        return this.View("~/Views/Privacy/Index.cshtml");
+    }
+}
