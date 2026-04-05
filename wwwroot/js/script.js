c16d3110d4337b83ed3ad9f7e099ecce0ce4cb65
Stanislav <sirichenkostas@gmail.com>
2026-04-05 19:50:17 +0300

    Adjusted some svg images and added js code.

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,4 +1,7 @@
-﻿function showSidebar() {
+﻿var acc = document.getElementsByClassName("question__header");
+var i;
+
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
 }
@@ -6,4 +9,16 @@
 function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
+}
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var question_answer = this.nextElementSibling;
+        if (question_answer.style.display === "block") {
+            question_answer.style.display = "none";
+        } else {
+            question_answer.style.display = "block";
+        }
+    });
 }
